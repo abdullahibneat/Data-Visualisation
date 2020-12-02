0a15ca8e820615d0492d08ec1810af350c713721
Abdullah Ibne Atiq <hello@abdu.io>
2020-12-02 11:16:41 +0000

    Show plots in main only

--- Plot 3 - daily visits time series.py
+++ Plot 3 - daily visits time series.py
@@ -28,19 +28,20 @@ for (name, value) in data.iteritems():
         category = 2
     categories_selected[category].append(name)
 
-# Show segmented data
-for i, selected in enumerate(categories_selected):
-    # Get all values from the selected column
-    # Use daily visitor data to create time series plot
-    data_selected = visitor_data[selected]
+if __name__ == "__main__":
+    # Show segmented data
+    for i, selected in enumerate(categories_selected):
+        # Get all values from the selected column
+        # Use daily visitor data to create time series plot
+        data_selected = visitor_data[selected]
 
-    # Display data as bar chart
-    plot = data_selected.plot()
+        # Display data as bar chart
+        plot = data_selected.plot()
 
-    # Set title, y-label
-    plot.set_title(categories[i] + " - visitors over the year 2019")
-    plot.set_ylabel("Visitors")
-    plot.set_ylim(bottom=0)
+        # Set title, y-label
+        plot.set_title(categories[i] + " - visitors over the year 2019")
+        plot.set_ylabel("Visitors")
+        plot.set_ylim(bottom=0)
 
-    # Show plots one at a time
-    plt.show()
+        # Show plots one at a time
+        plt.show()
